5915b70703084dad211c00abda6e6ce88033c45f
pickle <55500841+pickles9@users.noreply.github.com>
2019-09-25 11:36:31 -0400

    Initial upload

---
+++ a117_grades_adamschmok.py
@@ -0,0 +1,8 @@
+#   a117_grades.py
+#   This code is incomplete.
+my_courses = ["English", "Math", "CS"]
+
+check_grades = "y"
+while (check_grades == "y"):
+    for c in my_courses:
+        input("insert grade for "+ c)
