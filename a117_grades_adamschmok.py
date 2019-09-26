f275aa1e536bf841043fceabcf8f67d95eda2ae7
pickle <55500841+pickles9@users.noreply.github.com>
2019-09-26 11:36:29 -0400

    Completed code

--- a117_grades_adamschmok.py
+++ a117_grades_adamschmok.py
@@ -2,7 +2,24 @@
 #   This code is incomplete.
 my_courses = ["English", "Math", "CS"]
 
-check_grades = "y"
-while (check_grades == "y"):
-    for c in my_courses:
-        input("insert grade for "+ c)
+check_grades = True
+for course in my_courses:
+    print() # blank line
+    print("Enter your points for "+ course)
+    a = int(input("Points -> "))
+    if (a >= 90):
+        print("A")
+    elif (a >= 80):
+        print("B")
+    elif (a >= 70):
+    	print("C")
+    elif (a >= 60):
+    	print("D")
+    else:
+        print("F")
+        
+redo = input("Do you need to re-do these grades? (y/n)")
+if (redo == "y"):
+	check_grades = True
+else:
+	check_grades = False
